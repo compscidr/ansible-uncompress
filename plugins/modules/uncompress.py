d914cb7bcaffce57739ed2e0718a8111700794bd
Jason Ernst <ernstjason1@gmail.com>
2022-08-15 12:49:01 -0700

    remove namespace from definition

--- plugins/modules/uncompress.py
+++ plugins/modules/uncompress.py
@@ -21,7 +21,7 @@
 
 DOCUMENTATION = '''
 ---
-module: compscidr.uncompress
+module: uncompress
 version_added: 2.4
 short_description: Uncompresses an file after (optionally) copying it from the local machine.
 extends_documentation_fragment: files
